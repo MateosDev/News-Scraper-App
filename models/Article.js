f90169be59c943c3099fd61b6adcce754045464b
MateosDev <35548023+MateosDev@users.noreply.github.com>
2020-01-05 01:33:35 -0600

    both models done

---
+++ models/Article.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+
+//Save to Constructor
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    headline: {
+        type: String,
+        required: true
+    },
+
+    summary: {
+        type: String,
+        required: false
+    },
+
+    link: {
+        type: String,
+        required: true
+    },
+    comment: [{
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    }]
+})
+  //Creates Model from Schema using Mongoose Model Method
+  var Article = mongoose.model("Article", ArticleSchema);
+
+  module.exports = Article;
