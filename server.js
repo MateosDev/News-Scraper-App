4eeab83b48a07c15a83be4d915262a59d06b623d
MateosDev <35548023+MateosDev@users.noreply.github.com>
2020-01-05 01:02:50 -0600

    added a couple variables to help with testing

--- server.js
+++ server.js
@@ -9,4 +9,26 @@ var app = express();
 var port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Listening on port " + port));
 //How ya like that ES6 :P
-var MONGODB_URI
+
+//Parse into JSON - BodyParser not recommended
+app.use(logger("dev"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//MONGODB Connection
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var db = mongoose.connection;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+
+//CONNECT APP TO PUBLIC FOLDER 
+app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//IMPORT ROUTES FOR SERVER USE
+// var routes = require("./controllers/articlesController.js");
+// app.use(routes);
+
+//Server Listening / Start
+
