cbd755d800440fd267ba764ec4b1c0f4286b3049
MateosDev <35548023+MateosDev@users.noreply.github.com>
2020-01-05 05:26:01 -0600

    working on the controller file

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -2,3 +2,61 @@
 var express = require("express");
 var router = express.Router();
 var path = require("path");
+var cheerio = require("cheerio");
+var db = require("../models");
+var Comment = require("../models/Comments.js");
+var Article = require("../models/Article.js");
+
+//Route for ALL ARTICLES
+router.get("/", function(req,res){
+    //Grabs from Articles Collection
+    db.Article.find({})
+        .sort({ _id: -1})
+        .then(function(dbArticle){
+            //Sends Articles back to Client Side
+            res.render("index", { dbArticle });
+        })
+        .catch(function(err) {
+            //Sends Errors back to Client Side
+            res.json(err);
+        });
+});
+
+router.get("/scrape", function(res,req) {
+    //Use Axios To Grab Body
+    axios.get("https://associatedpress.com")//notice it doesnt have a / behind .com, not all will
+    .then(function(response) {
+        //load the body of HTML
+        var $ = cheerio.load(response.data);
+        //Loop anything through containing H4 Headline-Url tags using Cheerio
+        $(".c-entry-box--compact__title").each(function(i, element) {
+            var results = {};
+            //Saves H4 tags as headlines
+            results.headline = $(this)
+            .find("h3")
+            .html();
+            results.summary = $(this)
+            .find("p")
+            .html();
+            //h4s A Tag gets saved as URL
+            var aTag = $(this)
+                .find("a")
+                .attr("href");
+                results.url = "http://associatedpress.com" + aTag;
+                //New Article Creation
+                db.Article.create(results)
+                .then(function(dbArticle) {
+                    //check console for the result
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    //logs any errors
+                    console.log(err);
+                });
+
+        });
+        //Search/Scrape Complete Message to Client Side
+        res.send("Scrape is Complete");
+
+    })
+})
